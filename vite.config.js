ff0254f66cba5b85f9c63470cc898fecb2dfb888
Karthik7259 <mohankartik.cs23@rvce.edu.in>
2025-12-08 18:32:00 +0530

    multippages

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,21 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: 'src',
-  publicDir: '../public',
+  root: '.',
+  publicDir: 'public',
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
+        main: resolve(__dirname, 'index.html'),
+        rfid: resolve(__dirname, 'rfid-products.html'),
+        pcb: resolve(__dirname, 'pcb-design.html'),
+        simulations: resolve(__dirname, 'si-pi-em-simulations.html'),
+        silicon: resolve(__dirname, 'silicon-services.html'),
+        brady: resolve(__dirname, 'brady-products.html'),
+        av: resolve(__dirname, 'professional-av.html'),
+        trainings: resolve(__dirname, 'trainings.html'),
       },
     },
     minify: 'terser',
